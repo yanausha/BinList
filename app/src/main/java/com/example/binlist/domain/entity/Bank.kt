8dd2193fdcbe9ebb8aa9520ee14356ead1efa7c9
Yana Ushakova <junk7day@gmail.com>
2023-02-11 17:27:26 +0300

    add entity for domain layer

---
+++ app/src/main/java/com/example/binlist/domain/entity/Bank.kt
@@ -0,0 +1,8 @@
+package com.example.binlist.domain.entity
+
+data class Bank(
+    val city: String?,
+    val name: String?,
+    val phone: String?,
+    val url: String?
+)
---
+++ app/src/main/java/com/example/binlist/domain/entity/BinInfo.kt
@@ -0,0 +1,11 @@
+package com.example.binlist.domain.entity
+
+data class BinInfo(
+    val bank: Bank?,
+    val brand: String?,
+    val country: Country?,
+    val number: Number?,
+    val prepaid: Boolean?,
+    val scheme: String?,
+    val type: String?
+)
