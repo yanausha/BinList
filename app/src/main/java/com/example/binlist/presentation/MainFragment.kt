3cb1a2d84be3a46ac8dc99cd8be5c87ac1c7b726
Yana Ushakova <junk7day@gmail.com>
2023-02-11 17:46:18 +0300

    set main fragment

---
+++ app/src/main/java/com/example/binlist/presentation/MainFragment.kt
@@ -0,0 +1,56 @@
+package com.example.binlist.presentation
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.binlist.databinding.FragmentMainBinding
+import javax.inject.Inject
+
+class MainFragment : Fragment() {
+
+    private val component by lazy {
+        (requireActivity().application as BinApp).component
+    }
+
+    @Inject
+    lateinit var viewModelFactory: BinViewModelFactory
+    private val viewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[BinViewModel::class.java]
+    }
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding: FragmentMainBinding
+        get() = _binding ?: throw RuntimeException("FragmentMainBinding is null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onAttach(context: Context) {
+        component.inject(this)
+        super.onAttach(context)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+
+        fun newInstance() = MainFragment()
+    }
+}
